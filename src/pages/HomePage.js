cb8551d24c27cc3892a4f20395cf241430c0caa5
raonikeshnk <raonky222@gmail.com>
2024-06-24 20:12:45 +0530

    update 4 24 june

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,28 +2,14 @@ import React, { useState, useEffect } from "react";
 import Banner from "../components/Banner";
 import Body from "../components/Body";
 import "../App.scss";
-import Preloader from "../components/Preloader/Preloader";
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // Simulate content loading delay
-    setTimeout(() => {
-      setLoading(false); // Set loading to false after 2 seconds (adjust as per your needs)
-    }, 2000); // Adjust delay time as needed
-  }, []);
   return (
-    <>
-      {loading ? (
-        <Preloader />
-      ) : (
+
         <>
           <Banner />
           <Body />
         </>
-      )}
-    </>
   );
 };
 
